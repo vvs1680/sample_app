83ae307acc68b2779208ca0504ec40275f46c422
vvs <vvs1680@gmail.com>
2011-04-22 10:24:30 +0300

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -27,10 +27,10 @@ gem 'sqlite3-ruby', '1.3.1', :require => 'sqlite3'
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
 group :development do
-  gem 'rspec-rails', '2.0.1'
+  gem 'rspec-rails', '2.1.0'
 end
 
 group :test do
-  gem 'rspec', '2.0.1' 
+  gem 'rspec', '2.1.0' 
   gem 'webrat', '0.7.1'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,13 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+#  describe "GET 'home'" do
+#    it "should be have the right title" do
+#      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+#      get 'home'
+#    end
+#  end
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -15,5 +22,26 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+#  describe "GET 'contact'" do
+#    it "should be have the right title" do
+#      get 'contact'
+#      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+#    end
+#  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+#  describe "GET 'about'" do
+#    it "should be have the right title" do
+#      get 'about'
+#      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+#    end
+#  end
 
 end
